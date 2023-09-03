6fbd757a70020d398ae76b93f39e5debacf1a0a4
nish1013 <nishand2d@gmail.com>
2023-09-03 20:48:04 +0100

    Update array.map.ts

--- src/array.map.ts
+++ src/array.map.ts
@@ -20,9 +20,13 @@ interface Product {
   
   console.log(formattedProducts);
 // Output
-/* [
+/*
+
+[
     { id: 1, name: 'Laptop', price: '$999.99' },
     { id: 2, name: 'Smartphone', price: '$499.99' },
     { id: 3, name: 'Tablet', price: '$299.99' }
-  ] */
+] 
+
+*/
   
