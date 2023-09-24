f70f956deedf682f45ba514894450eb3cfe9af0c
nish1013 <nishand2d@gmail.com>
2023-09-24 17:05:28 +0100

    Fixed formatting

--- src/type.intersections.ts
+++ src/type.intersections.ts
@@ -7,17 +7,17 @@ type BaseUser = {
 
 // Define user roles with their respective permissions.
 type Admin = {
-  role: "admin";
+  role: 'admin';
   permissions: string[];
 };
 
 type Moderator = {
-  role: "moderator";
+  role: 'moderator';
   permissions: string[];
 };
 
 type Member = {
-  role: "member";
+  role: 'member';
 };
 
 // Define a type intersection to create a user type with roles and permissions.
@@ -26,36 +26,36 @@ export type User = BaseUser & (Admin | Moderator | Member);
 // Create user objects.
 const adminUser: User = {
   id: 1,
-  username: "admin123",
-  email: "admin@example.com",
-  role: "admin",
-  permissions: ["create", "update", "delete"],
+  username: 'admin123',
+  email: 'admin@example.com',
+  role: 'admin',
+  permissions: ['create', 'update', 'delete']
 };
 
 const moderatorUser: User = {
   id: 2,
-  username: "moderator456",
-  email: "moderator@example.com",
-  role: "moderator",
-  permissions: ["update"],
+  username: 'moderator456',
+  email: 'moderator@example.com',
+  role: 'moderator',
+  permissions: ['update']
 };
 
 const memberUser: User = {
   id: 3,
-  username: "member789",
-  email: "member@example.com",
-  role: "member",
+  username: 'member789',
+  email: 'member@example.com',
+  role: 'member'
 };
 
 // Function to check if a user has a specific permission.
 function hasPermission(user: User, permission: string): boolean {
-  if ("permissions" in user && user.permissions.includes(permission)) {
+  if ('permissions' in user && user.permissions.includes(permission)) {
     return true;
   }
   return false;
 }
 
 // Example usage:
-console.log(hasPermission(adminUser, "create")); // true
-console.log(hasPermission(moderatorUser, "delete")); // false
-console.log(hasPermission(memberUser, "update")); // false
+console.log(hasPermission(adminUser, 'create')); // true
+console.log(hasPermission(moderatorUser, 'delete')); // false
+console.log(hasPermission(memberUser, 'update')); // false
